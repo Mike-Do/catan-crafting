fe4ce6d5ff93708a818303240858c2b80e53b12c
Mike-Do <agentdonoso@gmail.com>
2023-12-04 17:56:42 -0500

    Add hex scene

--- main.js
+++ main.js
@@ -7,18 +7,92 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const smallHexGeometry = new THREE.CylinderGeometry( 0.95, 0.95, 1, 6 );
+const smallHexMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-camera.position.z = 5;
+// Array to hold smaller hexagon meshes
+const smallHexagons = [];
+
+// Function to create a single small hexagon
+function createSmallHexagon(x, y, z) {
+    const smallHexagonMesh = new THREE.Mesh(smallHexGeometry, smallHexMaterial);
+    smallHexagonMesh.position.set(x, y, z);
+    return smallHexagonMesh;
+}
+
+// Function to create the larger hexagon tile
+function createHexagonTile() {
+    const hexagonGroup = new THREE.Group();
+
+    // Define positions for smaller hexagons forming the larger hexagon
+    const positions = [
+        // [0, 0, 0],
+        // [1, 0, 0],
+        // [2, 0, 0],
+        // [0, 0, 1],
+        // [1, 0, 1],
+        // [2, 0, 1],
+        // [0, 0, 2],
+        // [1, 0, 2],
+        // [2, 0, 2]
+		// [0, 0, 0],
+		// [-1, 0, 0],
+		// [-2, 0, 0],
+
+		// [-0.5, 0, -1.5],
+		// [-0.5, 0, 1.5]
+
+		// GOOD NUMBERS
+		// [0, 0, 0],
+		// [1.5, 0, -0.866],
+		// [-1.5, 0, -0.866],
+		// [-3, 0, 0],
+		// [-1.5, 0, 0.866],
+		// [1.5, 0, 0.866],
+		// [3, 0, 0]
+
+		//
+		[-Math.sqrt(3), 0, 0],
+		[0, 0, 0],
+		[Math.sqrt(3)/2, 0, 1.5],
+		[-Math.sqrt(3)/2, 0, 1.5],
+		[Math.sqrt(3), 0, 0],
+		[Math.sqrt(3)/2, 0, -1.5],
+		[-Math.sqrt(3)/2, 0, -1.5]
+
+		// [-2, 0, -3],
+		// [2, 0, -3],
+
+    ];
+
+    // Create smaller hexagons at specified positions
+    for (let i = 0; i < positions.length; i++) {
+        const [x, y, z] = positions[i];
+        const smallHexagon = createSmallHexagon(x, y, z);
+        smallHexagons.push(smallHexagon);
+        hexagonGroup.add(smallHexagon);
+    }
+
+    return hexagonGroup;
+}
+
+// Add the larger hexagon tile to the scene
+const largerHexagon = createHexagonTile();
+scene.add(largerHexagon);
+
+// const cube = new THREE.Mesh( hexGeomgetry, material );
+// scene.add( cube );
+
+camera.position.z = 10;
+
+largerHexagon.rotation.x += 5;
+largerHexagon.rotation.y += 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	largerHexagon.rotation.x += 0.009;
+    largerHexagon.rotation.y += 0.009;
 
 	renderer.render( scene, camera );
 }
