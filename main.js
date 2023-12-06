6c8023bb65d0b1b772ce612e3ad305f9ec0df36b
Mike-Do <agentdonoso@gmail.com>
2023-12-06 17:29:05 -0500

    Add camera

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let camera;
+let controls;
+generateCamera();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+camera.position.set(0, 20, 100);
+// constrols.dampingFactor = 0.05;
+// controls.enableDamping = true;
+controls.update();
+
+
+
+function generateCamera() {
+    let fieldOfView = 75;
+    let aspect = window.innerWidth / window.innerHeight;
+    let nearPlane = 1;
+    let farPlane = 5000;
+    camera = new THREE.PerspectiveCamera(
+      fieldOfView,
+      aspect,
+      nearPlane,
+      farPlane
+    );
+    camera.position.x = 500;
+    camera.position.y = 500;
+    camera.position.z = 500;
+    camera.up.set(0, 1, 0);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    scene.add(camera);
+  }
 
 const smallHexGeometry = new THREE.CylinderGeometry( 0.95, 0.95, 1, 6 );
 const smallHexMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -91,8 +123,9 @@ largerHexagon.rotation.y += 5;
 function animate() {
 	requestAnimationFrame( animate );
 
-	largerHexagon.rotation.x += 0.009;
-    largerHexagon.rotation.y += 0.009;
+	// largerHexagon.rotation.x += 0.009;
+    // largerHexagon.rotation.y += 0.009;
+    controls.update();
 
 	renderer.render( scene, camera );
 }
